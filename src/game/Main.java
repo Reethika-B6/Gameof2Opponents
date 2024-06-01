0789c4756c7a54e437800993e7e3f76ba2af1dcd
Reethika <218r1a05b6@gmail.com>
2024-06-01 20:04:21 +0530

    First Commit

---
+++ src/game/Main.java
@@ -0,0 +1,14 @@
+package game;
+public class Main
+{
+   public static void main(String[] args)
+   {
+       
+   Player1 player=new Player1("Reethika","sword",100);
+   System.out.println(player.getName());
+   System.out.println(player.getWeapon());
+   System.out.println(player.getHealth());
+  
+    }
+    
+}
